e192830c161c2b62ad52d12b07de0b80fb762d52
Matt Imel <imel.matt.r@gmail.com>
2016-12-31 02:03:17 -0600

    Visual Fixes
    Hover events added on projects, Links enabled on downloads/external
    links.

--- js/projectinject.js
+++ js/projectinject.js
@@ -14,41 +14,38 @@
    exts([[String, String]]):    A list of external links to other sites relevant to the project, such as GitHub.
    gallery(String):             A folder path containing up to 6 images of the project.
 */
-var projectTemplate = function(name, desc, img, dwds, exts, gallery) {
-
+var projectTemplate = function (name, desc, img, dwds, exts, gallery) {
+    "use strict";
+    
     //Name of the project.
-    var n = name;
+    var n = name,
 
     //The description of the project. Will be put underneath the title.
-    var de = desc;
+    de = desc,
 
     //Image to be used as the background image of the project.
-    var i = img;
+    i = img,
 
     //Whether or not the project in the website is expanded (200px height, full suite) or
     //retracted (50px height, just title)
-    var expanded = true;
+    expanded = true,
+
+    //The position of this element in the webpage, 0 being at the top.
+    instance = projectTemplate.instances;
 
     //Increments the number of projects; used in id-ing various blocks.
-    projectTemplate.instances++;
+    projectTemplate.instances += 1;
 
     //Accessors
-    this.Name = function() { return n; };
-    this.Desc = function() { return de; };
-    this.ImgPath = function() { return i; };
+    this.Name = function () { return n; };
+    this.Desc = function () { return de; };
+    this.ImgPath = function () { return i; };
 
-    this.isExpanded = function() {
+    this.isExpanded = function () {
         return expanded;
     };
-    this.toggleExpanded = function() {
+    this.toggleExpanded = function () {
         expanded = !expanded;
-    };
-
-    //Adds a project to the list. Is only called once, from the main method.
-    this.printAsHTML = function() {
-        document.getElementById("projects_list").insertAdjacentHTML("beforeend",
-            prepHTMLInjection()
-        );
     };
 
     //Creates HTML blocks where a variable number must be inserted;
@@ -58,12 +55,12 @@ var projectTemplate = function(name, desc, img, dwds, exts, gallery) {
         //Create variable number of download links based on size of download array.
         var downloadStack = "";
         for (var x = 0; x < dwds.length; x++) {
-            downloadStack += "<div class=\"proj_downloadInst\"><img class=\"proj_downloadImg\" src=\"img/_icons/dwdIcon.png\" />" + dwds[x][0] + "<br /><h6>File Size</h6></div>"
+            downloadStack += "<a target=\"_blank\" href=" + dwds[x][1] + " class=\"proj_link_disablable\"><div class=\"proj_downloadInst\"><img class=\"proj_downloadImg\" src=\"img/_icons/dwdIcon.png\" />" + dwds[x][0] + "<br /><h6>File Size</h6></div></a>"
         }
 
         var externalStack = "";
         for(var x = 0; x < exts.length; x++) {
-            externalStack += "<div class=\"proj_extInst\">" + exts[x][0] + "</div>";
+            externalStack += "<a target=\"_blank\" href=\"" + exts[x][1] + "\" class=\"proj_link_disablable\"><div class=\"proj_extInst\">" + exts[x][0] + "</div></a>";
         }
 
         var galleryStack = "";
@@ -73,7 +70,7 @@ var projectTemplate = function(name, desc, img, dwds, exts, gallery) {
 
         return "<div class=\"proj_item_wrapper\" id=\"p_iw_" + projectTemplate.instances + "\">    \
             <div class=\"proj_upborder\"></div>                 \
-            <div class=\"proj_bkgd\">                           \
+            <div class=\"proj_bkgd\" style=\"background-image:url(" + img + ")\">                           \
                 <div class=\"proj_synopsis\">                   \
                     <h1 class=\"proj_title\">" + n + "</h1>     \
                     <h2 class=\"proj_desc\">" + de + "</h2>     \
@@ -91,6 +88,13 @@ var projectTemplate = function(name, desc, img, dwds, exts, gallery) {
             </div>                                              \
          </div>";
     };
+    
+    //Adds a project to the list. Is only called once, from the main method.
+    this.printAsHTML = function () {
+        document.getElementById("projects_list").insertAdjacentHTML("beforeend",
+            prepHTMLInjection()
+                                                                   );
+    };
 }
 
 projectTemplate.instances = 0;
@@ -99,18 +103,28 @@ projectTemplate.instances = 0;
 //the list, varying the sizes of the said projects via clicking, and managing the gallery
 //modals upon clicking an image.
 $(function() {
-    new projectTemplate("Asphodel Sky", "A very simple roguelike game with 2D graphics. Made in Java.", "../img/whatever.png", [
+    new projectTemplate("Asphodel Sky", "A very simple roguelike game with 2D graphics. Made in Java.", "img/asphodelsky/Background.png", [
         ["Download .jar", "LINK"],
         ["Download .zip", "LINK"]
     ], [["Github", "LINK"]], null).printAsHTML();
-    new projectTemplate("CoffeeChat", "Server and client software allowing versatile chat between multiple users. Comes with a set of user and admin commands. Made in Java.", "../img/whatever.png", [
+    new projectTemplate("CoffeeChat", "Server and client software allowing versatile chat between multiple users. Comes with a set of user and admin commands. Made in Java.", "img/coffeechat/Background.png", [
         ["Download .jar", "LINK"],
         ["Download .zip", "LINK"]
     ], [], "coffeechat").printAsHTML();
     new projectTemplate("This Website", "An intuitive, easy-to-navigate compilation of all my projects, including this one.", "../img/whatever.png", [], [], null).printAsHTML();
+    
+    //Initially disable links.
+    $("a.proj_link_disablable").click(function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        return false;
+    });
 
     //Expands and collapses each project.
     $(".proj_bkgd").click(function(e) {
+        //Do not reanimate while block is animating.
+        if($(this).is(":animated")) { return false; }
+
         if (!$(e.target).hasClass("proj_galleryImage") &&
             !$(e.target).hasClass("proj_downloadInst") &&
             !$(e.target).parent().hasClass("proj_galleryImage") && //Don't retract if image/download is clicked
@@ -118,6 +132,14 @@ $(function() {
             $(this).css("height") == "50px") { //Ignore when expanding
 
             if ($(this).css("height") == "50px") { //Opens
+                //Enables links.
+                $(this).find("a.proj_link_disablable").off();
+                
+                $(this).find("a.proj_link_disablable").css({
+                    "pointer-events": "auto",
+                    "cursor": "pointer"
+                });
+                
                 if($(window).width() > 1320) {
                     $(this).find(".proj_title").animate({
                         fontSize: "60px"
@@ -140,6 +162,19 @@ $(function() {
                 }, "slow");
 
             } else { //Closes
+                //Disables links.
+                $(this).find("a.proj_link_disablable").click(function(e) {
+                    e.preventDefault();
+                    e.stopImmediatePropagation();
+                    return false;
+                });
+                
+                $(this).find("a.proj_link_disablable").css({
+                    "pointer-events": "none",
+                    "cursor": "default"
+                });
+                
+                
                 $(this).animate({
                     height: "50px",
                     paddingTop: "0px"
@@ -155,29 +190,92 @@ $(function() {
         }
     });
 
-/*    $(".proj_galleryImage").click(function() { //On gallery icon click
+    ///MODAL///
+    var currentImage;
 
-        var verticalBound = $(window).height() * 0.6;
-        var horizontalBound = $(window).width() * 0.6;
+    function displayImageModal(element) {
+        var verticalAllowance = $(window).height() * 0.7;
+        var horizontalAllowance = $(window).width() * 0.7;
 
-        var aspectRatio = horizontalBound / verticalBound;
+        var verticalSize = $(element).find("img").get(0).naturalHeight;
+        var horizontalSize = $(element).find("img").get(0).naturalWidth;
 
-        //For the love of God, reorganize this mess
-        $("#modal_picture").css("height", $(window).height() * 0.6);
-        $("#modal_picture").css("width", $(window).width() * 0.6);
-        $("#modal_picture").css("margin-top", $(window).height() * 0.2);
-        $("#modal_picture").css("margin-left", $(window).width() * 0.2);
+        var reductionFactor;
+
+        if(verticalSize <= verticalAllowance && horizontalSize <= horizontalAllowance) {
+            reductionFactor = 1;
+        } else {
+            reductionFactor = Math.max(verticalSize/verticalAllowance, horizontalSize/horizontalAllowance);
+        }
+
+        var verticalCorrected = verticalSize/reductionFactor;
+        var horizontalCorrected = horizontalSize/reductionFactor;
+
         $("#modal_picture").css({
-            "background-image": "url(" + $(this).find("img").attr("src") + ")",
+            "height": verticalCorrected,
+            "width": horizontalCorrected,
+            "margin-top": (($(window).height() - verticalCorrected)/2),
+            "margin-left": (($(window).width() - horizontalCorrected)/2),
+            "background-image": "url(" + $(element).find("img").attr("src") + ")",
+            "background-size": (horizontalCorrected + "px " + verticalCorrected + "px"),
             "background-repeat": "no-repeat",
             "background-attachment": "fixed",
             "background-position": "center center"
         });
 
         $("#modal_wrapper").css("display", "block"); //Display modal
+    }
+
+    $(".proj_galleryImage").click(function() { //On gallery icon click
+        //A little convoluted; This if-statement makes sure that the modal doesn't
+        //open when the project tab is retracted; otherwise, the modal would still
+        //open if the user clicked where the icon would be.
+        if($(this).parent().parent().parent().height() > 50) {
+
+            currentImage = this;
+            displayImageModal(this);
+
+            if(currentImage.previousSibling && currentImage.previousSibling.nodeType != 1) {
+                $("#modal_left").css("display", "none");
+            }
+            if(currentImage.nextSibling && currentImage.nextSibling.nodeType != 1) {
+                $("#modal_right").css("display", "none");
+            }
+        }
     });
 
-    $("#modal_wrapper").click(function() { //Close modal
+    $("#modal_left").click(function() {
+        currentImage = currentImage.previousSibling;
+        displayImageModal(currentImage);
+
+        if(currentImage.previousSibling && currentImage.previousSibling.nodeType != 1) {
+            $("#modal_left").css("display", "none");
+        }
+        if(currentImage.nextSibling && currentImage.nextSibling.nodeType == 1) {
+            $("#modal_right").css("display", "block");
+        }
+    });
+
+    $("#modal_right").click(function() {
+        currentImage = currentImage.nextSibling;
+        displayImageModal(currentImage);
+
+        if(currentImage.nextSibling && currentImage.nextSibling.nodeType != 1) {
+            $("#modal_right").css("display", "none");
+        }
+        if(currentImage.previousSibling && currentImage.previousSibling.nodeType == 1) {
+            $("#modal_left").css("display", "block");
+        }
+    });
+
+    //Closes the modal if the outer region (i.e. not the picture or description)
+    //is clicked.
+    $("#modal_wrapper").click(function() {
         $("#modal_wrapper").css("display", "none");
-    }); */
+
+        $("#modal_left").css("display", "block");
+        $("#modal_right").css("display", "block");
+    }).children().click(function(e) {
+        return false;
+    });
 });
